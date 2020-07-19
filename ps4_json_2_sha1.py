4da550467131989b16c194c23030ef9815aebd96
hosamn <34396668+hosamn@users.noreply.github.com>
2020-07-19 10:34:12 +0200

    Add files via upload

---
+++ ps4_json_2_sha1.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+# Change the working directory to the py file dir:
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Get a list of current dir json files:
+for root, dirs, files in os.walk("."):
+    json_files = [f for f in files if f.endswith('.json')]
+
+# print(json_files)
+
+for json_file in json_files:
+
+    out_file_name = json_file.split('.')[0]+'.sha1'
+
+    with open(json_file) as opf:
+        data = json.loads(opf.read())
+
+    for i in range(data['numberOfSplitFiles']):
+        piece_name = data['pieces'][i]['url'].split('/')[-1]
+        piece_hash = data['pieces'][i]['hashValue']
+        file_line = piece_hash + ' *' + piece_name
+        with open(out_file_name, 'a') as outt:
+            outt.write(file_line+'\n')
